a0152e4d1d1cfbbbb1bbbeb2f73411d746a039fa
cosy247 <705194673@qq.com>
2025-01-24 17:10:18 +0800

    1

---
+++ src/components/Todo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Todo" :class="props.data.completed ? 'completed' : ''">
+		{{ props.data.content }}
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(['data']);
+</script>
+
+<style scoped>
+.Todo {
+	padding: 10px 20px;
+	border: 1px solid #e0e0e0;
+	border-radius: 10px;
+}
+.Todo.completed {
+	text-decoration: line-through;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="HomeView">
+    <div class="header flexRow">
+      <div class="title">{{ title }}</div>
+      <div class="flex1"></div>
+      <RouterLink class="tool" to="addTask">+</RouterLink>
+    </div>
+    <div class="todos flexColumn">
+      <Todo v-for="todo in todos" :data="todo"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Todo from '../components/Todo.vue';
+import { RouterLink } from 'vue-router';
+
+const title = ref('Today');
+
+const todos = ref([
+  { id: 1, content: 'Todo 1', completed: false, type: 'A', pre: '12133' },
+  { id: 2, content: 'Todo 2', completed: true, type: 'A', pre: '12133' },
+  { id: 1, content: 'Todo 1', completed: true, type: 'A', pre: '12133' },
+  { id: 2, content: 'Todo 2', completed: false, type: 'A', pre: '12133' },
+  { id: 3, content: 'Todo 3', completed: false, type: 'A', pre: '12133' },
+  { id: 1, content: 'Todo 1', completed: true, type: 'A', pre: '12133' },
+  { id: 2, content: 'Todo 2', completed: true, type: 'A', pre: '12133' },
+  { id: 3, content: 'Todo 3', completed: false, type: 'A', pre: '12133' },
+  { id: 1, content: 'Todo 1', completed: false, type: 'A', pre: '12133' },
+  { id: 2, content: 'Todo 2', completed: true, type: 'A', pre: '12133' },
+  { id: 3, content: 'Todo 3', completed: false, type: 'A', pre: '12133' },
+  { id: 1, content: 'Todo 1', completed: false, type: 'A', pre: '12133' },
+]);
+</script>
+
+<style scoped>
+.HomeView {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.header {
+  padding: 10px 20px;
+}
+.title {
+  font-size: 20px;
+  font-weight: 900;
+}
+.tool {
+  display: block;
+  height: 0;
+  width: 20px;
+  text-align: center;
+  line-height: 0;
+  color: white;
+  font-size: 20px;
+  font-weight: 900;
+  padding: 10px 0;
+  background: #3338;
+  border-radius: 4px;
+}
+.todos {
+  overflow: auto;
+  gap: 10px;
+  margin: 10px;
+}
+</style>
